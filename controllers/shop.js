68727e383ddbd4d3d7333faeb8f344e56bfcf3b5
shahvimal <shahvimal5009@gmail.com>
2019-04-01 21:38:13 +0530

    Frist Commit

---
+++ controllers/shop.js
@@ -0,0 +1,60 @@
+const Product = require("../models/product");
+
+exports.getProducts =(req, res, next) => {
+   Product.fetchAll((products)=>{
+      res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    });   
+};
+
+exports.getProduct = (req,res,next)=>{
+  const productId = req.params.productId;
+
+  Product.findById(productId,product=>{
+    res.render('shop/product-detail',{
+      product:product,
+      pageTitle:'Product Detail',
+      path:'/products'
+    });
+  })
+};
+
+exports.getIndex = (req,res,next)=>{
+  Product.fetchAll((products)=>{
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
+
+exports.getCart = (req,res,next)=>{
+  res.render('shop/cart',{
+    path:'/cart',
+    pageTitle:'Cart'
+  });
+};
+
+exports.postCart = (req,res,next)=>{
+  const prodId = req.body.productId;
+  console.log('product id',prodId);
+  res.redirect('/cart');
+}
+
+exports.getOrders = (req,res,next)=>{
+  res.render('shop/orders',{
+    path:'/orders',
+    pageTitle:'Orders'
+  });
+};
+
+exports.getCheckout = (req,res,next)=>{
+  res.render('shop/checkout',{
+    path:'/checkout',
+    pageTitle:'Checkout'
+  });
+};
